503c0256b2f9b4e91d95364777185deca738512e
wslmacieira <wslmacieira@outlook.com>
2024-06-28 16:04:32 -0300

    add serverless lambda ssr

---
+++ serverless.ts
@@ -0,0 +1,32 @@
+import 'zone.js/dist/zone-node';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import * as express from 'express';
+import { join } from 'path';
+import { AppServerModule } from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
+// The Express app is exported so that it can be used by serverless Functions.
+export const server = express();
+const distFolder = join(process.cwd(), 'dist/my-app/browser');
+const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+}));
+server.set('view engine', 'html');
+server.set('views', distFolder);
+// Example Express Rest API endpoints
+// server.get('/api/**', (req, res) => { });
+server.use(express.json())
+server.post('/api/**', (req, res) => {
+    res.send({ message: "hello world!" })
+});
+// Serve static files from /browser
+server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+}));
+// All regular routes use the Universal engine
+server.all('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+});
+export * from './src/main.server';
